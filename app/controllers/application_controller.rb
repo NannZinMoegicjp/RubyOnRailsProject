3b7bdd1c5cd71a1af5e38cff86ce00f3c902a036
NannZinMoeGIC <nannzinmoe@gicjp.com>
2023-06-19 14:50:43 +0630

    Added session tracker for dynmic source link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
     include DeviseWhitelist
+
+    before_action :set_source
+
+    def set_source
+        if params[:q]
+            session[:source] = params[:q]
+        end
+    end
 end
