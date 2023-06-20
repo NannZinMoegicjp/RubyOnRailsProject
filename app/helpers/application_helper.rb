57e1c414391ef9057d39b5617d87db204d9acb32
NannZinMoeGIC <nannzinmoe@gicjp.com>
2023-06-20 17:56:11 +0630

    Implemented content tag helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,16 @@ module ApplicationHelper
             (link_to "Login", new_user_session_path, class: "ms-2")
         end 
     end
+
+    def sample_helper
+        # "<div class='my-class'>My content</div>".html_safe
+        content_tag(:div, "My content", class: "my-class")
+    end
+
+    def source_helper(layout_name)
+        if session[:source]
+            greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name}"
+            content_tag(:p, greeting, class: "source-greeting")
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,4 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
