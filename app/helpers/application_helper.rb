54084c6afe14b57c9b6c6cf0ee0f0cebdcb21320
NannZinMoeGIC <nannzinmoe@gicjp.com>
2023-06-20 17:18:47 +0630

    Implemented view helper for auth links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def login_helper
+        if current_user.is_a?(User)
+            button_to "Logout", destroy_user_session_path, method: :delete,class: "link-button"
+        else
+            (link_to "Register", new_user_registration_path, class: "ms-2") + 
+            "<br>".html_safe +
+            (link_to "Login", new_user_session_path, class: "ms-2")
+        end 
+    end
 end
